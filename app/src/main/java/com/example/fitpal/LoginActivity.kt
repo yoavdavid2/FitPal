0cbfd8a36571bac59dce7c062aa2ba6a48864fbe
Ori Eluk <ori42410@gmail.com>
2025-03-14 20:59:53 +0200

    feat: added login logic

--- app/src/main/java/com/example/fitpal/LoginActivity.kt
+++ app/src/main/java/com/example/fitpal/LoginActivity.kt
@@ -1,20 +1,29 @@
 package com.example.fitpal
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : Activity() {
+
 
     private lateinit var emailInput: EditText
     private lateinit var passwordInput: EditText
     private lateinit var loginButton: Button
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
 
         emailInput = findViewById(R.id.email_input)
         passwordInput = findViewById(R.id.password_input)
@@ -23,7 +32,36 @@ class LoginActivity : Activity() {
         loginButton.setOnClickListener {
             val email = emailInput.text.toString()
             val password = passwordInput.text.toString()
-            Log.i("Test Credentials", "Email: $email, Password: $password")
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please enter all the fields", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success
+                        val user = mAuth.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                        updateUI(null)
+                    }
+                }
+
+        }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java) // Replace with your main activity
+            startActivity(intent)
+            finish()
+        } else {
+            Log.i("LoginActivity", "Login failed or user is null.")
         }
     }
 
