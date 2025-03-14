013f51cdb24ad55d23efa23941a913e99506df87
Ori Eluk <ori42410@gmail.com>
2025-03-14 21:18:20 +0200

    feat: added google authentication to login page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,4 +73,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("com.google.firebase:firebase-auth:22.0.0")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
+
 }
+
+
