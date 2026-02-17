f7ba97803f281b3fbdacedfe8f836cd6257cada3
Ori Eluk <ori42410@gmail.com>
2026-02-17 22:33:37 +0200

    wip: trying to fix firebase 403 errors

--- app/src/main/java/com/example/fitpal/base/MyApplication.kt
+++ app/src/main/java/com/example/fitpal/base/MyApplication.kt
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
 
 class MyApplication: Application() {
     @SuppressLint("StaticFieldLeak")
@@ -15,5 +17,8 @@ class MyApplication: Application() {
         super.onCreate()
         Globals.context = applicationContext
         FirebaseApp.initializeApp(this)
+        FirebaseAppCheck.getInstance().installAppCheckProviderFactory(
+            DebugAppCheckProviderFactory.getInstance()
+        )
     }
 }
