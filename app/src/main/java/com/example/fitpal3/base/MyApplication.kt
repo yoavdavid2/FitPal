505b2f21d0243709a898c553e1b606d23f575094
Ori Eluk <ori42410@gmail.com>
2026-02-20 16:34:56 +0200

    fix: first google login crushes app

--- app/src/main/java/com/example/fitpal3/base/MyApplication.kt
+++ app/src/main/java/com/example/fitpal3/base/MyApplication.kt
@@ -3,9 +3,13 @@ package com.example.fitpal3.base
 import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
+import com.google.firebase.Firebase
 import com.google.firebase.FirebaseApp
 import com.google.firebase.appcheck.FirebaseAppCheck
 import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.firestoreSettings
+import com.google.firebase.firestore.memoryCacheSettings
 
 class MyApplication: Application() {
     @SuppressLint("StaticFieldLeak")
@@ -15,6 +19,11 @@ class MyApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        val db = Firebase.firestore
+        val settings = firestoreSettings {
+            setLocalCacheSettings(memoryCacheSettings { })
+        }
+        db.firestoreSettings = settings
         Globals.context = applicationContext
         FirebaseApp.initializeApp(this)
         FirebaseAppCheck.getInstance().installAppCheckProviderFactory(
